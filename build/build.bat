8ad183e40d47d1c3ba2b7334259417e8c07971c7
Ben Ramey <ben.ramey@gmail.com>
2013-03-11 11:56:57 -0500

    Adds build/packaging setup

---
+++ build/build.bat
@@ -0,0 +1,8 @@
+rmdir /Q /S .\buildoutput
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:ResolveReferences;_WPPCopyWebApplication /p:BuildingProject=true;OutDir=..\build\buildoutput\;configuration=Release ../GoodlyFere.Import.Web/GoodlyFere.Import.Web.csproj
+
+cd ..\build
+"C:\Program Files (x86)\WinZip\winzip32.exe" -a .\ImportWebPackage_%1.zip .\buildoutput\_PublishedWebsites\GoodlyFere.Import.Web 
+
+rmdir /Q /S .\buildoutput
