af2fb827b50e99c5c8d173f097cee5aed17fe693
Ben Ramey <ben.ramey@gmail.com>
2013-04-04 14:58:43 -0500

    Refactors messages a little bit

--- GoodlyFere.Import.Web/Resources/js/app.js
+++ GoodlyFere.Import.Web/Resources/js/app.js
@@ -4,9 +4,12 @@
         this.router.navigate(fragment, options || { trigger: true });
     },
     showError: function(message) {
-        $("#messages").html("ERROR: " + message);
+        _showMessage(message, "error");
     },
     showInfo: function (message) {
-        $("#messages").html("INFO: " + message);
+        _showMessage(message, "info");
+    },
+    _showMessage: function(message, type) {
+        $("#messages").append("<div class='"+type.toLowerCase()+"'>"+type.toUpperCase()+": " + message + "</div>");
     }
 });
